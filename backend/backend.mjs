d78d32f78d544e47bc56e69a3e5d1faa2dce0913
emma-bdrs <emma.badarous@edu.univ-fcomte.fr>
2026-03-10 15:36:26 +0100

    commit

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("http://127.0.0.1:8090");
+export const pb = new PocketBase("https://festival.badarous.fr/_/");
 
 export async function getArtistsSortedByRepresentationDate(collection = "artiste") {
 	const artists = await pb.collection(collection).getFullList();
